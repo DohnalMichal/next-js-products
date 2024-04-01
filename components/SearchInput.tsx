ec497268ced949130ad2492856f3d518e533d26a
DohnalMichal <michal.dohnal7@gmail.com>
2024-04-01 08:47:26 +0200

    Sync SearchInput value with query params

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,40 +1,37 @@
 'use client'
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { debounce } from 'lodash'
-import { type ChangeEvent, useState } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
+import type { ChangeEvent } from 'react'
 
 const SearchInput = () => {
-  const [value, setValue] = useState('')
-
-  const router = useRouter()
+  const { replace } = useRouter()
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
-  const debouncedRouterPush = debounce((url: string) => {
-    router.push(url)
-  }, 200)
+  const handleChange = useDebouncedCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value
+      const params = new URLSearchParams(searchParams.toString())
+      params.set('skip', '0')
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.target.value
-    setValue(newValue)
-    const params = new URLSearchParams(searchParams.toString())
+      if (newValue.length === 0) {
+        params.delete('q')
+      } else {
+        params.set('q', newValue)
+      }
 
-    if (newValue.length === 0) {
-      params.delete('q')
-    } else {
-      params.set('q', newValue)
-    }
-
-    const url = pathname + '?' + params.toString()
-    debouncedRouterPush(url)
-  }
+      const url = `${pathname}?${params.toString()}`
+      replace(url)
+    },
+    300,
+  )
 
   return (
     <input
       type="text"
       name="query"
-      value={value}
+      defaultValue={searchParams.get('q')?.toString()}
       placeholder="Search for products..."
       className="w-full mt-4 p-2 border border-gray-400/30 rounded-md shadow-md"
       onChange={handleChange}
